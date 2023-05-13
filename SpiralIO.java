d4d005203cf1d35a25d9597f2c10c8cc7f40013c
kalaimanikaliyaperumal <75480214+kalaimanikaliyaperumal@users.noreply.github.com>
2023-05-13 16:45:12 +0530

    Update SpiralIO.java

--- SpiralIO.java
+++ SpiralIO.java
@@ -8,6 +8,7 @@ public class SpiralIO {
 
 	public static void main(String[] args) {
 		int[][] matrix = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+		//int[][] matrix = { { 1, 2 }, { 3, 4 } };
 		printSpiral(matrix);
 	}
 
@@ -20,36 +21,36 @@ public class SpiralIO {
 
 		while ((minRow <= maxRow) && (minCol <= maxCol)) {
 
-			// System.out.println("min column to max column");
+			System.out.println("min column to max column");
 			for (int i = minCol; i <= maxCol; i++) {
-				// System.out.println(arr[minRow][i]);
+				System.out.println(arr[minRow][i]);
 				outPut.add(arr[minRow][i]);
 				// System.out.println(outPut.toString());
 			}
 			minRow++;
 
-			// System.out.println("min row to max row");
+			System.out.println("min row to max row");
 			for (int j = minRow; j <= maxRow; j++) {
-				// System.out.println(arr[j][maxCol]);
+				System.out.println(arr[j][maxCol]);
 				outPut.add(arr[j][maxCol]);
 				// System.out.println(outPut.toString());
 			}
 			maxCol--;
 
-			if (minRow >= maxRow || minCol >= maxCol)
+			if (minRow > maxRow || minCol > maxCol)
 				break;
 
-			// System.out.println("max column to min column");
+			System.out.println("max column to min column");
 			for (int k = maxCol; k >= minCol; k--) {
-				// System.out.println(arr[maxRow][k]);
+				System.out.println(arr[maxRow][k]);
 				outPut.add(arr[maxRow][k]);
 				// System.out.println(outPut.toString());
 			}
 			maxRow--;
 
-			// System.out.println("max row to min row");
+			System.out.println("max row to min row");
 			for (int l = maxRow; l >= minRow; l--) {
-				// System.out.println(arr[l][minCol]);
+				System.out.println(arr[l][minCol]);
 				outPut.add(arr[l][minCol]);
 				// System.out.println(outPut.toString());
 			}
