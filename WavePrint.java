44afd29e8eaddc392947909d403eaf8a01391e7c
kalaimanikaliyaperumal <75480214+kalaimanikaliyaperumal@users.noreply.github.com>
2023-06-14 17:48:27 +0530

    Update WavePrint.java

--- WavePrint.java
+++ WavePrint.java
@@ -1,4 +1,4 @@
-..package src;
+//package src;
 
 import java.util.Scanner;
 
@@ -63,3 +63,13 @@ class WavePrint {
 		WavePrint(R, C, arr);
 	}
 }
+
+// Sample Input
+// 4
+// 4
+// 1 2 3 4
+// 5 6 7 8
+// 9 10 11 12
+// 13 14 15 16
+// Sample Answer
+// 1 5 9 13 14 10 6 2 3 7 11 15 16 12 8 4 
